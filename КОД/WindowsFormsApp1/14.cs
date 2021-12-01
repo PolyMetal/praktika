5f0e19aa6d7772b547048a0778aa310c56f0d1b3
PolyMetal <95376885+PolyMetal@users.noreply.github.com>
2021-12-01 21:32:15 +0300

    Add files via upload

---
+++ КОД/WindowsFormsApp1/14.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int s = Convert.ToInt32(textBox1.Text);
+            }
+            catch (System.FormatException)
+            {
+                DialogResult dialogResult = MessageBox.Show("Вы ввели символ! Пожалуйста,введите цифрy");
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int s = Convert.ToInt32(textBox1.Text);
+            }
+            catch (System.FormatException)
+            {
+                DialogResult dialogResult = MessageBox.Show("Вы ввели символ! Пожалуйста,введите цифрy");
+            }
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int s = Convert.ToInt32(textBox1.Text);
+            }
+            catch (System.FormatException)
+            {
+                DialogResult dialogResult = MessageBox.Show("Вы ввели символ! Пожалуйста,введите цифрy");
+            }
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            int Summa = Convert.ToInt32(textBox1.Text);
+            int Month = Convert.ToInt32(textBox2.Text);
+            double percent = 1.18;
+            double number = Summa * percent;
+            textBox4.Text = Convert.ToString(number);
+            double EverMonth = number / Month;
+            textBox5.Text = Convert.ToString(EverMonth);
+
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Текстовый файл (*.txt)|*.txt";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    using (StreamWriter sw = new StreamWriter(myStream))
+                    {
+                        sw.WriteLine($"Сумма кредита " + Summa + " умножается на ставку" + percent + " и получается: сумма кредита с процентами =  " + number + " далее, делим её " + number + " на количестов месяцев " + Month + " и получаем: ежемесячный платёж =  " + EverMonth);
+                    }
+                    myStream.Close();
+                }
+            }
+
+        }
+
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int s = Convert.ToInt32(textBox1.Text);
+            }
+            catch (System.FormatException)
+            {
+                DialogResult dialogResult = MessageBox.Show("Вы ввели символ! Пожалуйста,введите цифрy");
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int s = Convert.ToInt32(textBox1.Text);
+            }
+            catch (System.FormatException)
+            {
+                DialogResult dialogResult = MessageBox.Show("Вы ввели символ! Пожалуйста,введите цифрy");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ КОД/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.IO.Stream;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            ulong Summa = Convert.ToUInt64(textBox1.Text);
+            ushort Month = Convert.ToUInt16(textBox2.Text);
+            double percent = 1.18;
+            double number = Summa * percent;
+            textBox3.Text = Convert.ToString(number);
+            double EverMonth = number / Month;
+            textBox4.Text = Convert.ToString(EverMonth);
+
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e) //сумма
+        {
+
+        }
+
+        public void textBox2_TextChanged(object sender, EventArgs e) //срок
+        {
+
+        }
+
+        public void textBox3_TextChanged(object sender, EventArgs e) //проценты
+        {
+
+        }
+
+        public void textBox4_TextChanged(object sender, EventArgs e) //сумма кредита с процентами
+        {
+
+        }
+
+        public void textBox5_TextChanged(object sender, EventArgs e) //ежемесячный платёж
+        {
+
+        }
+        public void button2_Click(object sender, EventArgs e)
+        {
+            ulong Summa = Convert.ToUInt64(textBox1.Text);
+            ushort Month = Convert.ToUInt16(textBox2.Text);
+            double percent = 1.18;
+            double number = Summa * percent;
+            textBox3.Text = Convert.ToString(number);
+            double EverMonth = number / Month;
+            textBox4.Text = Convert.ToString(EverMonth);
+
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Текстовый файл (*.txt)|*.txt";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = (Stream)saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    using (StreamWriter sw = new StreamWriter(myStream))
+                    {
+                        sw.WriteLine("Сумма кредита с процентами" + number + "Ежемесячный платеж" + EverMonth);
+                    }
+                    myStream.Close();
+                }
+            }
+        }
+    }
+}
